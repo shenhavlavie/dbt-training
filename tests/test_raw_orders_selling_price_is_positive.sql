15d112d904168698c17a62b3d24840713218cf2d
shenhav.mmm@gmail.com <shenhav.mmm@gmail.com>
2023-01-21 17:11:14 +0000

    tets working

--- tests/test_raw_orders_selling_price_is_positive.sql
+++ tests/test_raw_orders_selling_price_is_positive.sql
@@ -1,9 +1,9 @@
 with orders as (
-select * from {{ ref('raw_orders') }}
+select * from {{ ref('raw_order') }}
 )
 Course Notes 6
-select orderid,
-sum(ordersellingprice) as total_sp
+select ORDERID,
+sum(ORDERSELLINGPRICE) as total_sp
 from orders
 group by orderid
 having total_sp<0
